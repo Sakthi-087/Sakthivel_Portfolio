d4b4e82e25125fc7a361e3c53d12854258c86b54
Sakthi-087 <svel00937@gmail.com>
2025-12-28 22:00:40 +0530

    Setup Vite + React portfolio project

--- src/components/models/hero_models/HeroExperience.jsx
+++ src/components/models/hero_models/HeroExperience.jsx
@@ -1,79 +1,43 @@
-import { useGSAP } from "@gsap/react";
-import gsap from "gsap";
+import { OrbitControls } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+import { useMediaQuery } from "react-responsive";
 
-import AnimatedCounter from "../components/AnimatedCounter";
-import Button from "../components/Button";
-import { words } from "../constants";
-import HeroExperience from "../components/models/hero_models/HeroExperience";
+import { Room } from "./Room";
+import HeroLights from "./HeroLights";
+import Particles from "./Particles";
+import { Suspense } from "react";
 
-const Hero = () => {
-    useGSAP(() => {
-        gsap.fromTo(
-            ".hero-text h1",
-            { y: 50, opacity: 0 },
-            { y: 0, opacity: 1, stagger: 0.2, duration: 1, ease: "power2.inOut" }
-        );
-    });
+const HeroExperience = () => {
+    const isMobile = useMediaQuery({ query: "(max-width: 768px)" });
+    const isTablet = useMediaQuery({ query: "(max-width: 1024px)" });
 
     return (
-        <section id="hero" className="relative overflow-hidden">
-            <div className="absolute top-0 left-0 z-10">
-                <img src="/images/bg.png" alt="" />
-            </div>
+        <Canvas camera={{ position: [0, 0, 15], fov: 45 }}>
+            {/* deep blue ambient */}
+            <ambientLight intensity={0.2} color="#1a1a40" />
+            {/* Configure OrbitControls to disable panning and control zoom based on device type */}
+            <OrbitControls
+                enablePan={false} // Prevents panning of the scene
+                enableZoom={!isTablet} // Disables zoom on tablets
+                maxDistance={20} // Maximum distance for zooming out
+                minDistance={5} // Minimum distance for zooming in
+                minPolarAngle={Math.PI / 5} // Minimum angle for vertical rotation
+                maxPolarAngle={Math.PI / 2} // Maximum angle for vertical rotation
+            />
 
-            <div className="hero-layout">
-                {/* LEFT: Hero Content */}
-                <header className="flex flex-col justify-center md:w-full w-screen md:px-20 px-5">
-                    <div className="flex flex-col gap-7">
-                        <div className="hero-text">
-                            <h1>
-                                Shaping
-                                <span className="slide">
-                  <span className="wrapper">
-                    {words.map((word, index) => (
-                        <span
-                            key={index}
-                            className="flex items-center md:gap-3 gap-1 pb-2"
-                        >
-                        <img
-                            src={word.imgPath}
-                            alt="person"
-                            className="xl:size-12 md:size-10 size-7 md:p-2 p-1 rounded-full bg-white-50"
-                        />
-                        <span>{word.text}</span>
-                      </span>
-                    ))}
-                  </span>
-                </span>
-                            </h1>
-                            <h1>into Real Projects</h1>
-                            <h1>that Deliver Results</h1>
-                        </div>
-
-                        <p className="text-white-50 md:text-xl relative z-10 pointer-events-none">
-                            Hi, I’m Adrian, a developer based in Croatia with a passion for
-                            code.
-                        </p>
-
-                        <Button
-                            text="See My Work"
-                            className="md:w-80 md:h-16 w-60 h-12"
-                            id="counter"
-                        />
-                    </div>
-                </header>
-
-                {/* RIGHT: 3D Model or Visual */}
-                <figure>
-                    <div className="hero-3d-layout">
-                        <HeroExperience />
-                    </div>
-                </figure>
-            </div>
-
-            <AnimatedCounter />
-        </section>
+            <Suspense fallback={null}>
+                <HeroLights />
+                <Particles count={100} />
+                <group
+                    scale={isMobile ? 0.7 : 1}
+                    position={[0, -3.5, 0]}
+                    rotation={[0, -Math.PI / 4, 0]}
+                >
+                    <Room />
+                </group>
+            </Suspense>
+        </Canvas>
     );
 };
 
-export default Hero;
+export default HeroExperience;
